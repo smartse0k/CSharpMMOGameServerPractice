ca36070cb84659ffe1c3142ef31e45c797f7f965
smartse0k <smartse0k@naver.com>
2022-07-19 21:30:17 +0900

    Util 프로젝트 생성, Serializer 구현

---
+++ Util/Serializer.cs
@@ -0,0 +1,47 @@
+﻿namespace Util
+{
+    public class Serializer
+    {
+        private Queue<Task> queue;
+        private bool isProcessing;
+
+        public Serializer()
+        {
+            this.queue = new Queue<Task>();
+            this.isProcessing = false;
+        }
+
+        public void Enqueue(Task task)
+        {
+            this.queue.Enqueue(task);
+
+            // TODO : Thread Safe
+            if (!this.isProcessing)
+            {
+                this.isProcessing = true;
+                this.Process();
+            }
+        }
+
+        private void Process()
+        {
+            Task task = this.queue.Dequeue();
+            if (task == null)
+            {
+                return;
+            }
+
+            task.Start();
+            task.ContinueWith((Task t) =>
+            {
+                if (this.queue.Count == 0)
+                {
+                    this.isProcessing = false;
+                    return;
+                }
+
+                this.Process();
+            });
+        }
+    }
+}
